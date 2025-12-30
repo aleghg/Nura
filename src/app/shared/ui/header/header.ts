266eee8e22c9db7894c712f50ca07c1e37acb06b
aleghg <magutierrez@tcc.com.co>
2025-12-30 10:15:46 -0500

    Se agrega encabezado transparente con animaciones de desplazamiento

--- src/app/shared/ui/header/header.ts
+++ src/app/shared/ui/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,4 +8,13 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+
+  isScrolled =false; 
+
+  @HostListener('window:scroll')
+  onScroll(){
+    this.isScrolled = window.scrollY >50;
+    
+  }
+}
