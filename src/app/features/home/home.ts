66659c137d87070613db01a69f4890793f193281
aleghg <magutierrez@tcc.com.co>
2025-12-30 10:02:20 -0500

    Se añadio animaciones de desplazamiento y heroe editorial

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+
+import { Component, AfterViewInit } from '@angular/core';
+
 
 @Component({
+  standalone: true,
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    const elements = document.querySelectorAll('.reveal');
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    }, { threshold: 0.2 });
+
+    elements.forEach(el => observer.observe(el));
+  }
 }
