00d3818837d2510e0efefd2a0b96f9d07fca0345
aleghg <magutierrez@tcc.com.co>
2026-01-16 09:26:58 -0500

    Renderizacion login y register

--- src/app/auth/auth-guard.ts
+++ src/app/auth/auth-guard.ts
@@ -1,6 +1,7 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
-import { AuthService } from './auth-service';
+import { AuthService } from '../../services/auth.service';
+
 
 
 
